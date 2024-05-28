8841ac8403ad52c2d1bf72cb15c59856458c5f0f
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-28 23:42:39 +0200

    Add Output subproject

---
+++ Output/output.py
@@ -0,0 +1,23 @@
+# here is an comment! below there are different outputs
+
+# you can use the newline escape sequence
+print("Hello\nWorld!")
+
+# you can use doublequotes and single quotes in special ways
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+# as you see doublequotes and singlequotes can be used interchangeably
+
+# you can easily concatenate strings
+print("Hello" + "America!")
+
+# to add the space between you have to type it
+print("Hello" + " " + "America!")
+# or
+print("Hello " + "America!")
+# or
+print("Hello" + " America!")
+
+# you have to be cautious because indentation really matters in python
