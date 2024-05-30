1977d75d03dd9007843968bc824a65ec2b21ce97
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-30 07:52:38 +0200

    Add Input subproject

---
+++ Input/input.py
@@ -0,0 +1,7 @@
+# to input a stream we simply use input function
+input("Enter the value here: ")
+
+# the above input only takes the input stream, but doesn't store it
+
+# you can combine print and input functions with concatenation operator
+print("Hello " + input("What is your name? "))
