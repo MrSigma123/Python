4cdb3d2a2fe6311a2a3a1e37e439f619f54fc1b2
MrSigma123 <karol.czolpinski@gmail.com>
2024-06-05 23:59:09 +0200

    Add Arithmetic subproject

---
+++ Arithmetic/arithmetic.py
@@ -0,0 +1,22 @@
+# getting familiar with Python arithmetic syntax
+
+# basic operands such as + - * / are standard
+# python uses PEMDAS standard, so there is defined power operator
+
+var1 = 1
+var2 = 2
+
+sum = var1 + var2
+difference = var1 - var2
+multiplication = var1 * var2
+division = var1 / var2
+power_of_var2 = var2 ** 2
+
+print("var1 = 1, var2 = 2")
+print(f"The sum of var1 and var2 is {sum}")
+print(f"The difference of var1 and var2 is {difference}")
+print(f"The multiplication of var1 and var2 is {multiplication}")
+print(f"The division of var1 and var2 is {division}")
+print(f"The power of var2 raised to the second power is {power_of_var2}")
+
+
