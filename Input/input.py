766eee668b50b890ffffcaba750fc8ee49205b29
MrSigma123 <karol.czolpinski@gmail.com>
2024-06-06 22:01:11 +0200

    Add f-string example

--- Input/input.py
+++ Input/input.py
@@ -5,3 +5,12 @@ input("Enter the value here: ")
 
 # you can combine print and input functions with concatenation operator
 print("Hello " + input("What is your name? "))
+
+# in python you can use special kind of strings called f-strings
+# f string is a literal string, prefixed with 'f', which contains 
+# expressions inside braces
+
+# in the following code the value variable value will be placed
+# instead {value}
+value = 1
+print(f"Here is the value variable value is {value}")
