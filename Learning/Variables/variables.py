3b47dd13f267b91cc241b51a4ef17a501f55bafe
MrSigma123 <karol.czolpinski@gmail.com>
2024-06-13 23:59:30 +0200

    Add Variables subproject

---
+++ Learning/Variables/variables.py
@@ -0,0 +1,8 @@
+# variables in python
+
+# in python in order to create a variable you just have to type the
+# variable identifier and assign the value to that particular variable
+# there is no need to define the variable type, example below
+
+var_string = "Ho ho ho, this is a string!"
+print(var_string)
